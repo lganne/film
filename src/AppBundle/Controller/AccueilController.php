ddb2742ed7436442cb27090abcc1f104fa0aa985
unknown <lganne2@yahoo.fr>
2015-01-11 19:02:23 +0100

    affichage liste film et detail

--- src/AppBundle/Controller/AccueilController.php
+++ src/AppBundle/Controller/AccueilController.php
@@ -12,6 +12,24 @@ class AccueilController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $em=$this->getDoctrine()->getManager();
+        $repo=$em->getrepository("AppBundle:Movie");
+        $data=$repo->findby(array(),array("note"=>"DESC"),54);
+       // print_r($data);
+        return $this->render('default/index.html.twig',array('liste'=>$data));
     }
+    /**
+     * @Route("/detail/{id}", name="detail")
+     */
+    public function detailAction($id)
+    {
+        if (isset($id))
+        {
+            $em=$this->getDoctrine()->getManager();
+        $repo=$em->getrepository("AppBundle:Movie");
+        $data=$repo->find($id);
+        }
+         return $this->render('default/detail.html.twig',array("detail"=>$data));
+    }
+            
 }
